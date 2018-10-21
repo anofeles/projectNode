41d0ff5bd449dbc3458581688e413621a578a69d
kaxam <kaxam1@gmail.com>
2018-10-21 11:51:33 +0400

    nodejs dasawyisi

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const staticAsset = require('static-asset');
+const mongoose = require('mongoose');
 
-var Post = require('./models/post');
+const config = require('./config');
+const routes = require('./routes');
 
-// const arr = ['rrrr', 'tttt', 'yyyyy'];
-
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.get('/', function (req, res) {
-    Post.find({}).then(post =>res.render('index', {post: post}))
-});
-
-//res.render('index', {arr: arr});
-app.get('/create', function (req, res) {res.render('create');});
-app.post('/create', function (req, res) {
-    var{title,body} = req.body;
-
-    Post.create({
-        title: title,
-        body: body
+// database
+mongoose.Promise = global.Promise;
+mongoose.set('debug', config.IS_PRODUCTION);
+mongoose.connection
+    .on('error', error => console.log(error))
+    .on('close', () => console.log('Database connection closed.'))
+    .once('open', () => {
+        const info = mongoose.connections[0];
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
     });
-    res.redirect('/');
+mongoose.connect(config.MONGO_URL, { useMongoClient: true });
+
+// express
+const app = express();
+
+// sets and uses
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(staticAsset(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    '/javascripts',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
+
+// routers
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.use('/api/auth', routes.auth);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-module.exports = app;
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.render('error', {
+        message: error.message,
+        error: !config.IS_PRODUCTION ? error : {}
+    });
+});
+
+app.listen(config.PORT, () =>
+    console.log(`Example app listening on port ${config.PORT}!`)
+);
